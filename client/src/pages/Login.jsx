7206720c8dd5ec2ceff93a511a5e8ce8dbc7dfdf
Chaps0218 <mathiguevara@gmail.com>
2023-07-30 11:27:26 -0500

    Arreglado el login
    Login funcional con la interfaz. Falta arreglar detalles visuales

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,40 +1,76 @@
-import React, { useState } from 'react';
-import './styles.css'; // Importa el archivo de estilos
-import Popup from '../components/Popup.jsx';
+import { useForm } from "react-hook-form";
+import { useAuth } from "../context/AuthContext";
+import { Link, useNavigate } from "react-router-dom";
+import { useEffect, useState } from "react";
+import Popup from "../components/Popup";
 
-const Login = () => {
+function Login() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+  const { signin, isAuthenticated, errors: registerErrors } = useAuth();
+  const navigate = useNavigate();
+  const onSubmit = handleSubmit((data) => {
+    signin(data);
+  });
+
+  useEffect(() => {
+    if (isAuthenticated) {setShowPopup(true);}
+
+  }, [isAuthenticated]);
   const [showPopup, setShowPopup] = useState(false);
-
-  const handleLogin = () => {
-    // Aquí puedes agregar la lógica para el inicio de sesión
-    setShowPopup(true);
-  };
 
   const handleClosePopup = () => {
     setShowPopup(false);
+    navigate("/profile");
   };
 
   return (
     <div className="login-container">
       <div className="login-image"></div>
-      <div className="login-form">
-        <h1>FORMULARIO DE ADMISIÓN PARA DOCENTES</h1>
-        <label>Email</label>
-        <input type="text" />
-        <label>Contraseña</label>
-        <input type="password" />
-        <button type="submit" onClick={handleLogin}>
-          Login
-        </button>
-        <p onClick={() => window.location.href = '/registro'}>Ir a Registro</p>
+      <div className="bg-zinc-600 max-w-md w-full p-10 rounded-md">
+        {registerErrors.map((error, i) => (
+          <div key={i} className="bg-red-500 p-2 text-white">
+            {error}
+          </div>
+        ))}
+        <h1 className="text-2xl font-bold">Login</h1>
+        <form onSubmit={onSubmit}>
+          <input
+            type="text"
+            {...register("email", { required: true })}
+            className="w-full bg-zinc-700 text-white px-4 py-2 rounded-md border m-3 "
+            placeholder="Correo"
+          />
+          {errors.email && (
+            <p className="text-red-500">El correo es requerido</p>
+          )}
+
+          <input
+            type="password"
+            {...register("password", { required: true })}
+            className="w-full bg-zinc-700 text-white px-4 py-2 rounded-md border m-3"
+            placeholder="Contraseña"
+          />
+          {errors.password && (
+            <p className="text-red-500">La contraseña es requerida</p>
+          )}
+          <button type="submit">Login</button>
+        </form>
+        <p className="flex gap-x-2 justify-between mt-3">
+          No tienes una cuenta<Link to="/register" className=" text-sky-500">Registrarse</Link>
+        </p>
+        {showPopup && (
+          <Popup
+            mensaje="Tu información será manipulada conforme a la necesidad de la institución sin lugar a reclamos, conforme a la ley de protección de datos, etc."
+            ruta="/plataforma" // Aquí pasa la ruta que desees redireccionar desde el componente Login
+            onClose={handleClosePopup}
+          />
+        )}
       </div>
-      {showPopup && (
-        <Popup
-          mensaje="Tu información será manipulada conforme a la necesidad de la institución sin lugar a reclamos, conforme a la ley de protección de datos, etc."
-          ruta="/plataforma" // Aquí pasa la ruta que desees redireccionar desde el componente Login
-          onClose={handleClosePopup}
-        />
-      )}
+
     </div>
   );
 };
