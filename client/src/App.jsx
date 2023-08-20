92e5b87060ead52633ae1cf3ba674a64a21a2187
Mila <carivera14@espe.edu.ec>
2023-08-19 22:39:58 -0500

    postulacion

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -30,7 +30,7 @@ import InformacionPersonal from "./pages/InformacionPersonal";
 
 import ListaPostulantes from "./pages/ListaPostulantes";
 
-import Actividad from "./components/CRUD/Departamento";
+import Actividad from "./components/CRUD/Postulacion";
 
 const App = () => {
   const [isDarkMode, setIsDarkMode] = useState(false);
