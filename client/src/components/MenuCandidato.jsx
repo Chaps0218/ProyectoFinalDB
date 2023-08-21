c86248054061b0a9993b14020eaeb23a67e2fa26
Chaps0218 <mathiguevara@gmail.com>
2023-08-21 16:58:26 -0500

    Cambios de CSS

--- client/src/components/MenuCandidato.jsx
+++ client/src/components/MenuCandidato.jsx
@@ -7,25 +7,28 @@ const Menu = ({ title, subtitle1, subtitle2, subtitle3, icon1, icon2, icon3 }) =
     const { isAuthenticated, logout, user } = useAuth();
     return (
         <div className="menu">
-            <h1 className="menu-title">
-                {isAuthenticated ? (
-                    <>
-                        {user && user.name1 ? (
-                            <div>{user.name1} {user.name2} {user.lastname1} {user.lastname2}</div>
-                        ) : (
-                            <li>Loading...</li>
-                        )}
-                    </>
-                ) : (
-                    null
-                )}</h1>
-            <hr className="menu-divider" />
-            <a href="/inicioPostulante"><FiHome /> Inicio</a>
-            <a href="/informacionPostulante"><FiUser /> {subtitle1} <span>{icon1}</span></a>
-            <a href="/postulacion"><FiFile />{subtitle2}<span>{icon2}</span></a>
-            <a href="/informacion"><FiFolder /> {subtitle3}<span>{icon3}</span></a> 
-            {/* aquí poner el popup */}
-            <button className="menu-button" onClick={() => {logout();}}>Cerrar sesión</button>
+            <div>
+                <h1 className="menu-title">
+                    {isAuthenticated ? (
+                        <>
+                            {user && user.name1 ? (
+                                <div>{user.name1} {user.name2} {user.lastname1} {user.lastname2}</div>
+                            ) : (
+                                <li>Loading...</li>
+                            )}
+                        </>
+                    ) : (
+                        null
+                    )}</h1>
+                <hr className="menu-divider" />
+            </div>
+            <div>
+                <a href="/inicioPostulante"><FiHome /> Inicio</a>
+                <a href="/informacionPostulante"><FiUser /> {subtitle1} <span>{icon1}</span></a>
+                <a href="/postulacion"><FiFile />{subtitle2}<span>{icon2}</span></a>
+                <a href="/informacion"><FiFolder /> {subtitle3}<span>{icon3}</span></a>
+            </div>
+            <button className="menu-button" onClick={() => { logout(); }}>Cerrar sesión</button>
         </div>
 
     );
--- client/src/components/Tabla.jsx
+++ client/src/components/Tabla.jsx
@@ -268,8 +268,9 @@ export default function App() {
               className="bg-transparent outline-none text-default-400 text-small"
               onChange={onRowsPerPageChange}
             >
+              <option value="1">1</option>
+              <option value="5">5</option>
               <option value="10">10</option>
-              <option value="15">15</option>
               <option value="20">20</option>
             </select>
           </label>
